8c7de995897f1db8e61ff4d7e380317a77d69bae
JaiKarvir <jkarvir@scu.edu>
2018-11-25 16:22:30 -0800

    Updated clipboard to identify text and images

--- app/src/main/java/jai/clipboard/ClipboardAdapter.java
+++ app/src/main/java/jai/clipboard/ClipboardAdapter.java
@@ -2,6 +2,7 @@ package jai.clipboard;
 
 import android.app.Activity;
 import android.content.Context;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,6 +10,7 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.ToggleButton;
@@ -19,20 +21,21 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.pramod.firebase.R;
 import com.pramod.firebase.storage.ClipHistory;
 import com.pramod.firebase.storage.ClipHistoryStore;
+import com.pramod.firebase.util.KeyStore;
 
 import java.util.ArrayList;
 import java.util.Map;
 
-public class ClipboardAdapter  extends ArrayAdapter<ClipHistory> {
+public class ClipboardAdapter extends ArrayAdapter<ClipHistory> {
 
     Activity context;
     int layoutId;
     ArrayList<ClipHistory> clipContents = new ArrayList<ClipHistory>();
     FirebaseDatabase fdb = FirebaseDatabase.getInstance();
-    private static String key = "clipboard/"+FirebaseAuth.getInstance().getCurrentUser().getUid()+"/history";
+    private static String key = KeyStore.getClipboardHistoryKeyForUser();
 
     public ClipboardAdapter(Activity context, int layoutId, ArrayList<ClipHistory> clipContents) {
-        super(context, layoutId,clipContents);
+        super(context, layoutId, clipContents);
         this.context = context;
         this.layoutId = layoutId;
         this.clipContents = clipContents;
@@ -41,56 +44,68 @@ public class ClipboardAdapter  extends ArrayAdapter<ClipHistory> {
     public View getView(final int position, View view, ViewGroup parent) {
 
         View row = view;
-        ClipboardDetails clipboard = null;
-
-
-        if(row == null){
-            LayoutInflater inflater = ((Activity) context).getLayoutInflater();
-            row = inflater.inflate(layoutId, parent, false);
-            clipboard = new ClipboardDetails();
-            clipboard.deviceTitleTxtView = (TextView) row.findViewById(R.id.device_title);
-            clipboard.clipboardContentTxtView =(TextView) row.findViewById(R.id.clipboard_content);
-            clipboard.delBtn = (ImageButton) row.findViewById(R.id.btn_delete);
-
-            row.setTag(clipboard);
-        } else {
-            clipboard =(ClipboardDetails) row.getTag();
-        }
 
         ClipHistory clipDetails = clipContents.get(position);
-        clipboard.deviceTitleTxtView.setText(clipDetails.getDeviceName());
-        clipboard.clipboardContentTxtView.setText(clipDetails.getClipContent());
 
-       clipboard.delBtn.setOnClickListener(new View.OnClickListener() {
-           @Override
-           public void onClick(View v) {
-               Toast.makeText(context, "Delete button Clicked", Toast.LENGTH_LONG).show();
-               ClipHistory clipHistory = clipContents.get(position);
-               ClipHistoryStore storeObject = new ClipHistoryStore();
-               Map<String,ClipHistory> map = storeObject.getClipContents();
-               String mapKey = clipHistory.getTimestamp();
-               map.remove(mapKey);
-               clipContents.remove(position);
+        LayoutInflater inflater = ((Activity) context).getLayoutInflater();
+        row = inflater.inflate(layoutId, parent, false);
 
-               DatabaseReference dbReference = fdb.getReference(key).child(mapKey);
-               dbReference.removeValue();
+        TextView device_title_txt = (TextView) row.findViewById(R.id.device_title);
+        device_title_txt.setText(clipDetails.getDeviceName());
 
-               notifyDataSetChanged();
-           }
-       });
+        if(clipDetails.getMessageType().equals("1")){
+            /*TextView clip_content_txt = (TextView) row.findViewById(R.id.clipboard_content);
+            clip_content_txt.setText(clipDetails.getClipContent());*/
+            RelativeLayout single_clip_layout = row.findViewById(R.id.single_clip_layout);
+            TextView clip_content_txt = new TextView(this.getContext());
+            clip_content_txt.setId(R.id.clipboard_content);
+            clip_content_txt.setTextSize(20);
+            clip_content_txt.setText(clipDetails.getClipContent());
+            clip_content_txt.setAutoSizeTextTypeWithDefaults(clip_content_txt.AUTO_SIZE_TEXT_TYPE_UNIFORM);
+
+            RelativeLayout.LayoutParams lp = new RelativeLayout.LayoutParams(400,RelativeLayout.LayoutParams.WRAP_CONTENT);
+            lp.addRule(RelativeLayout.RIGHT_OF, R.id.img_device);
+            lp.addRule(RelativeLayout.BELOW, R.id.device_title);
+
+            clip_content_txt.setLayoutParams(lp);
+            single_clip_layout.addView(clip_content_txt);
+
+        }else if(clipDetails.getMessageType().equals("2")){
+            RelativeLayout single_clip_layout = row.findViewById(R.id.single_clip_layout);
+            ImageView clip_content_img = new ImageView(this.getContext());
+            clip_content_img.setId(R.id.clipboard_content);
+            clip_content_img.setImageResource(R.drawable.image2);
+
+            RelativeLayout.LayoutParams lp = new RelativeLayout.LayoutParams(400,RelativeLayout.LayoutParams.WRAP_CONTENT);
+            lp.addRule(RelativeLayout.RIGHT_OF, R.id.img_device);
+            lp.addRule(RelativeLayout.BELOW, R.id.device_title);
+
+            clip_content_img.setLayoutParams(lp);
+            single_clip_layout.addView(clip_content_img);
+        }
 
 
 
+        ImageButton del_btn = (ImageButton) row.findViewById(R.id.btn_delete);
+
+        del_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(context, "Delete button Clicked", Toast.LENGTH_LONG).show();
+                ClipHistory clipHistory = clipContents.get(position);
+                ClipHistoryStore storeObject = new ClipHistoryStore();
+                Map<String, ClipHistory> map = storeObject.getClipContents();
+                String mapKey = clipHistory.getTimestamp();
+                map.remove(mapKey);
+                clipContents.remove(position);
+
+                DatabaseReference dbReference = fdb.getReference(key).child(mapKey);
+                dbReference.removeValue();
+
+                notifyDataSetChanged();
+            }
+        });
         return row;
-
     }
 
-
-
-
-    static class ClipboardDetails {
-        TextView deviceTitleTxtView;
-        TextView clipboardContentTxtView;
-        ImageButton delBtn;
-    }
 }
